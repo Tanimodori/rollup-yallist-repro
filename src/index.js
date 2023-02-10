febfff6d887a2b1d69d9782a47a94e4fd3ffdbb3
Tanimodori <unknowner2014@gmail.com>
2023-02-10 15:29:08 +0800

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import * as itor from "yallist/iterator";
-
-console.log(itor);
+console.log(async () => {
+  const module = await import("electron-updater");
+  const autoUpdater =
+    module.autoUpdater ||
+    // @ts-expect-error Hotfix for https://github.com/electron-userland/electron-builder/issues/7338
+    module.default.autoUpdater;
+  return autoUpdater.checkForUpdatesAndNotify();
+});
