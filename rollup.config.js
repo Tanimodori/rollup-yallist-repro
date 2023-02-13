6fc60cac72cf4f04db4ebc7db0d1b0eab486447f
Tanimodori <unknowner2014@gmail.com>
2023-02-13 14:48:54 +0800

    fix: output cjs

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ module.exports = {
   input: "src/index.js",
   output: {
     dir: "dist",
-    format: "esm",
+    format: "cjs",
     exports: "named",
     preserveModules: true,
     entryFileNames: (info) => `${info.name}.js`,
