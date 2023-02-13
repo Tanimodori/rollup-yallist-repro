e351d12db9fbfffbc2faed42beda224ce8ce05c6
Tanimodori <unknowner2014@gmail.com>
2023-02-13 14:34:42 +0800

    refactor: using simple index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 console.log(async () => {
   const module = await import("electron-updater");
-  const autoUpdater =
-    module.autoUpdater ||
-    // @ts-expect-error Hotfix for https://github.com/electron-userland/electron-builder/issues/7338
-    module.default.autoUpdater;
-  return autoUpdater.checkForUpdatesAndNotify();
+  return module;
 });
