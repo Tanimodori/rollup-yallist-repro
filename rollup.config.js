1727b2b9bd865aac18804f79cdf67ecd960bb698
Tanimodori <unknowner2014@gmail.com>
2023-02-13 14:43:55 +0800

    deps: add builtin-modules, electron-builtins

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
+const nodeBuiltins = require("builtin-modules/static");
+const electronBuiltins = require("electron-builtins");
 
 module.exports = {
   input: "src/index.js",
@@ -9,6 +11,15 @@ module.exports = {
     exports: "named",
     preserveModules: true,
     entryFileNames: (info) => `${info.name}.cjs`,
+  },
+  external: (src) => {
+    const [name] = src.split("/");
+    const externalNames = [
+      ...nodeBuiltins,
+      ...nodeBuiltins.map((name) => `node:${name}`),
+      ...electronBuiltins,
+    ];
+    return externalNames.includes(name);
   },
   plugins: [resolve(), commonjs()],
 };
